08fcd18a341a18625d91cc65b219c0a3040b70e0
allen <alienwing1999@gmail.com>
2017-07-05 13:33:56 -0500

    initial commit for ronald

---
+++ Ronald/Ronald2.py
@@ -0,0 +1,9 @@
+from math import *
+import ccircle
+
+window = ccircle.Window("Hi", 600, 400)
+window.clear(0,1,1)
+width=12
+height=12
+window.drawRect(10, 100,width,height, 1, 1, 1)
+window.update()
